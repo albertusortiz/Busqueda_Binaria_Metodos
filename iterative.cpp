405c13710d188085a283ef38cc26a5466b66e10f
albertusortiz <alberto.ortiz.vargas@gmail.com>
2020-11-25 11:42:15 -0600

    Implementado version recursiva de busqueda binaria.

--- iterative.cpp
+++ iterative.cpp
@@ -2,17 +2,7 @@
 using namespace std;
 
 int binarySearchRecursive(int array[], int left, int right, int objective) {
-    if (right >= left) {
-        int mid = left + (right - left) / 2;
-        if (array[mid] == objective) {
-            return mid;
-        }
-        if (array[mid] > objective) {
-            return binarySearchRecursive(array, left, mid - 1, objective);
-        }
-        return binarySearchRecursive(array, mid + 1, right, objective);
-    }
-    return -1;
+    
 }
 
 int main() {
