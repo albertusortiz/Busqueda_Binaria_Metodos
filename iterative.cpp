8679c0d188189d225b4f43d1f61458922d73d6e7
albertusortiz <alberto.ortiz.vargas@gmail.com>
2020-11-25 11:47:05 -0600

    Agregando metodo iterativo para la busqueda binaria.

--- iterative.cpp
+++ iterative.cpp
@@ -2,7 +2,13 @@
 using namespace std;
 
 int binarySearchRecursive(int array[], int left, int right, int objective) {
-    
+    while(left <= right) {
+        int mid = left + (right - left) / 2;
+        if (array[mid] == objective) return mid;
+        if (array[mid] < objective) left = mid + 1;
+        else right = mid - 1;
+    }
+    return -1;
 }
 
 int main() {
